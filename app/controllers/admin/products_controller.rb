0d6f2cbcf12be9f6c860bba4354fc47a12d07da2
Navdeep Singh <navdeep5@ualberta.ca>
2024-08-19 18:22:30 -0600

    Creating and adding products to storage

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,13 @@
-class Admin::ProductsController < ApplicationController
+class Admin::ProductsController < AdminController
   before_action :set_admin_product, only: %i[ show edit update destroy ]
 
   # GET /admin/products or /admin/products.json
   def index
-    @admin_products = Admin::Product.all
+    # if params[:query].present?
+    #   @pagy, @admin_products = pagy(Product.where("name LIKE ?", "%#{params[:query]}%"))
+    # else
+    #   @pagy, @admin_products = pagy(Product.all)
+    # end
   end
 
   # GET /admin/products/1 or /admin/products/1.json
@@ -12,7 +16,7 @@ class Admin::ProductsController < ApplicationController
 
   # GET /admin/products/new
   def new
-    @admin_product = Admin::Product.new
+    @admin_product = Product.new
   end
 
   # GET /admin/products/1/edit
@@ -21,7 +25,7 @@ class Admin::ProductsController < ApplicationController
 
   # POST /admin/products or /admin/products.json
   def create
-    @admin_product = Admin::Product.new(admin_product_params)
+    @admin_product = Product.new(admin_product_params)
 
     respond_to do |format|
       if @admin_product.save
@@ -36,14 +40,16 @@ class Admin::ProductsController < ApplicationController
 
   # PATCH/PUT /admin/products/1 or /admin/products/1.json
   def update
-    respond_to do |format|
-      if @admin_product.update(admin_product_params)
-        format.html { redirect_to admin_product_url(@admin_product), notice: "Product was successfully updated." }
-        format.json { render :show, status: :ok, location: @admin_product }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @admin_product.errors, status: :unprocessable_entity }
+    @admin_product = Product.find(params[:id])
+    if @admin_product.update(admin_product_params.reject { |k| k["images"] })
+      if admin_product_params["images"]
+        admin_product_params["images"].each do |image|
+          @admin_product.images.attach(image)
+        end
       end
+      redirect_to admin_products_path, notice: "Product updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -60,11 +66,11 @@ class Admin::ProductsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_admin_product
-      @admin_product = Admin::Product.find(params[:id])
+      @admin_product = Product.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def admin_product_params
-      params.require(:admin_product).permit(:name, :description, :price, :category_id, :active)
+      params.require(:product).permit(:name, :description, :price, :category_id, :active, images: [])
     end
 end
